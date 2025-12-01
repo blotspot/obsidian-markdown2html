45d71aba83dcbdf9771909a57fdc42d39d548d46
blotspot <blotspot@protonmail.com>
2025-12-01 18:37:46 +0100

    added txt copy / context menu options

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const PLAINTXT_ACTION_TEXT = "Copy as Text";
+export const PLAINTXT_ICON = "markdown2txt-icon";
+export const HTML2CLIP_ACTION_TEXT = "Copy as HTML";
+export const MD2HTML_ICON = "markdown2html-icon";
--- src/plugin.ts
+++ src/plugin.ts
@@ -4,14 +4,24 @@ import {
 	App,
 	debounce,
 	Editor,
+	getFrontMatterInfo,
 	MarkdownRenderer,
 	MarkdownView,
+	Menu,
 	Modal,
 	Notice,
 	Plugin,
+	removeIcon,
 	setIcon,
+	TFile,
 } from "obsidian";
 import { Markdown2HtmlSettings, Markdown2HtmlSettingsTab as Markdown2HtmlSettingsTab } from "./settings";
+import {
+	HTML2CLIP_ACTION_TEXT as MD2HTML_ACTION_TEXT,
+	MD2HTML_ICON,
+	PLAINTXT_ACTION_TEXT,
+	PLAINTXT_ICON,
+} from "./constants";
 
 export default class Markdown2Html extends Plugin {
 	private copyInProgressModal: Modal;
@@ -20,11 +30,19 @@ export default class Markdown2Html extends Plugin {
 	async onload() {
 		// add custom icon
 		addIcon(
-			"markdown2html-icon",
+			MD2HTML_ICON,
 			`<g transform="scale(4.1666)" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round">
 			<path d="M17.5 2 17.5 8 15 6 17.5 8 20 6M4.11 8 4.11 2 7.85 5 11.5 2 11.5 8"/>
 			<path d="M8.25 16 5.25 19 8.25 22M11.25 22 12.75 16M15.75 16 18.75 19 15.75 22"/>
 			<path d="M2,12L22,12"/>
+			</g>`
+		);
+		addIcon(
+			PLAINTXT_ICON,
+			`<g transform="scale(4.1666)" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round">
+			<path d="M2 12h20"/>
+    	<path d="M4.5 22v-6M7 16H2M10 22l4-6M14 22l-4-6M19.5 22v-6M22 16h-5"/>
+			<path d="M17.4 2v5.98l-2.492-1.994L17.4 7.98l2.491-1.994M4.109 8V2.02l3.737 2.99L11.583 2v6"/>
 			</g>`
 		);
 
@@ -38,33 +56,74 @@ export default class Markdown2Html extends Plugin {
 		const rotateDiv = createDiv({ parent: this.copyInProgressModal.contentEl, cls: "md2html-rotate" });
 		setIcon(rotateDiv, "loader");
 
-		const copyCallback = () => {
+		//// Add commands
+		// callback function to get the content provider (editor or file)
+		const getContentProvider = (): Editor | TFile | null => {
 			const view = this.app.workspace.getActiveViewOfType(MarkdownView);
 			if (view != null) {
-				if (view.editor != null) {
-					this.startCopyProcess();
-					this.renderHtml(view.editor);
-				} else {
-					this.copyResult = view.contentEl;
-					if (this.hasCopyResult()) {
-						this.startCopyProcess();
-						this.copyHtmlToClipboard(settingsTab.settings);
-					}
-				}
+				return view.editor;
 			}
+			const file = this.app.workspace.activeEditor?.file;
+			if (file != null) {
+				return file;
+			}
+			return null;
 		};
 
-		// add copy command
+		// register copy commands
 		this.addCommand({
 			id: "clipboard",
-			icon: "markdown2html-icon",
-			name: "Copy editor selection or full note as HTML",
-			callback: copyCallback,
+			icon: MD2HTML_ICON,
+			name: MD2HTML_ACTION_TEXT,
+			checkCallback: (checking: boolean) => {
+				const contentProvider = getContentProvider();
+				if (checking) {
+					return !!contentProvider;
+				}
+				this.renderMarkdownToHtml(contentProvider as Editor | TFile);
+				return true;
+			},
+		});
+		this.addCommand({
+			id: "txtclipboard",
+			icon: PLAINTXT_ICON,
+			name: PLAINTXT_ACTION_TEXT,
+			checkCallback: (checking: boolean) => {
+				const contentProvider = getContentProvider();
+				if (checking) {
+					return !!contentProvider;
+				}
+				this.copyPlainTextToClipboard(contentProvider as Editor | TFile);
+				return true;
+			},
 		});
 
-		// add ribon icon
-		this.addRibbonIcon("markdown2html-icon", "Copy editor selection or full note as HTML", () => copyCallback());
+		// add ribon action
+		this.addRibbonIcon(MD2HTML_ICON, MD2HTML_ACTION_TEXT, () => {
+			const contentProvider = getContentProvider();
+			if (contentProvider !== null) {
+				this.renderMarkdownToHtml(contentProvider);
+			}
+		});
 
+		// register context menu events
+		const addMenuItems = (menu: Menu, contentProvider: Editor | TFile) => {
+			menu.addSeparator();
+			this.addHtmlMenuItem(menu, contentProvider);
+			this.addTextMenuItem(menu, contentProvider);
+			menu.addSeparator();
+		};
+
+		// editor menu
+		this.registerEvent(this.app.workspace.on("editor-menu", (menu, editor, view) => addMenuItems(menu, editor)));
+		// file menu
+		this.registerEvent(
+			this.app.workspace.on("file-menu", (menu, file) => {
+				if (file instanceof TFile) {
+					addMenuItems(menu, file);
+				}
+			})
+		);
 		// register post processor to monitor markdown render progress
 		this.registerMarkdownPostProcessor(async (el, ctx) => {
 			// INFO:
@@ -76,36 +135,87 @@ export default class Markdown2Html extends Plugin {
 				// this function will execute effectively only once after all rendering actions are fully done
 				this.copyHtmlToClipboard(settingsTab.settings);
 			}
-		}, Number.MAX_SAFE_INTEGER);
+		}, Number.MAX_SAFE_INTEGER /** using a high order number to make sure this renderer goes last at every step */);
 	}
 
-	/** Openes a modal to let the user know that the copy is in progress and triggers the render of the markdown document or selection. */
-	private renderHtml = async (editor: Editor) => {
-		const path = this.app.workspace.activeEditor?.file?.path ?? "";
-		const content = () => {
-			if (editor.somethingSelected()) {
-				return editor.getSelection();
-			} else {
-				return editor.getValue();
-			}
-		};
+	private addHtmlMenuItem(menu: Menu, contentProvider: Editor | TFile) {
+		menu.addItem(item => {
+			item
+				.setTitle(MD2HTML_ACTION_TEXT)
+				.setIcon(MD2HTML_ICON)
+				.onClick(async () => this.renderMarkdownToHtml(contentProvider));
+		});
+	}
 
-		console.debug("Markdown2Html: Copying to clipboard", path);
-		await MarkdownRenderer.render(this.app, content(), this.copyResult as HTMLElement, path, this);
-	};
+	private addTextMenuItem(menu: Menu, contentProvider: Editor | TFile) {
+		menu.addItem(item => {
+			item
+				.setTitle(PLAINTXT_ACTION_TEXT)
+				.setIcon(PLAINTXT_ICON)
+				.onClick(async () => this.copyPlainTextToClipboard(contentProvider));
+		});
+	}
 
-	/** Cleans up the rendered HTML and stores it in the system clipboard. */
+	/**
+	 * Cleans up the rendered HTML and stores it in the system clipboard.
+	 * Executes after a short delay to make sure all steps of the rendering are done.
+	 * Each time the method is called within the delay, the timer resets.
+	 */
 	private copyHtmlToClipboard = debounce(
 		async (settings: Markdown2HtmlSettings) => {
-			navigator.clipboard
-				.writeText(await cleanHtml(this.copyResult as HTMLElement, settings))
-				.then(() => new Notice("HTML copied to the clipboard", 3500))
-				.catch(() => new Notice("Couldn't copy html to the clipboard", 3500))
-				.finally(() => this.endCopyProcess());
+			if (this.hasCopyResult()) {
+				navigator.clipboard
+					.writeText(await cleanHtml(this.copyResult as HTMLElement, settings))
+					.then(() => new Notice("HTML copied to the clipboard", 3500))
+					.catch(e => {
+						console.error("MD2HTML: Error while copying HTML to the clipboard", e);
+						new Notice("Couldn't copy HTML to the clipboard", 3500);
+					})
+					.finally(() => this.endCopyProcess());
+			}
 		},
 		500 /* wait delay until copy to clipboard happens */,
 		true /* reset delay if method is called before timer finishes */
 	);
+
+	private async getContent(contentProvider: Editor | TFile) {
+		if (contentProvider instanceof Editor) {
+			return contentProvider.somethingSelected() ? contentProvider.getSelection() : contentProvider.getValue();
+		} else {
+			return this.app.vault.read(contentProvider);
+		}
+	}
+
+	/** triggers the render of the markdown note or selection. */
+	private async renderMarkdownToHtml(contentProvider: Editor | TFile) {
+		try {
+			this.startCopyProcess();
+			const content = await this.getContent(contentProvider);
+			const path = this.app.workspace.activeEditor?.file?.path ?? "";
+			MarkdownRenderer.render(this.app, content, this.copyResult as HTMLElement, path, this);
+		} catch (e) {
+			console.error("MD2HTML: Error while rendering HTML", e);
+			new Notice("Error while rendering HTML", 3500);
+			this.endCopyProcess();
+		}
+	}
+
+	private async copyPlainTextToClipboard(contentProvider: Editor | TFile) {
+		this.startCopyProcess();
+		if (this.hasCopyResult()) {
+			const fileContents = await this.getContent(contentProvider);
+			const fmtInfo = getFrontMatterInfo(fileContents);
+
+			navigator.clipboard
+				.writeText((fmtInfo.exists ? fileContents.slice(fmtInfo.contentStart) : fileContents).trim())
+				.then(() => new Notice("Text copied to the clipboard", 3500))
+				.catch(e => {
+					console.error("MD2HTML: Error while copying Text to the clipboard", e);
+					new Notice("Couldn't copy Text to the clipboard", 3500);
+				})
+				.finally(() => this.endCopyProcess());
+		}
+	}
 
 	private startCopyProcess() {
 		this.copyResult = createDiv();
@@ -121,5 +231,8 @@ export default class Markdown2Html extends Plugin {
 		return this.copyResult !== undefined;
 	}
 
-	onunload() {}
+	onunload() {
+		removeIcon(MD2HTML_ICON);
+		removeIcon(PLAINTXT_ICON);
+	}
 }
